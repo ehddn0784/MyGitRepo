66029cbcc0ea47b5a09180759767f5c6629ecc56
ehddn0784 <ehddn0784@naver.com>
2019-10-29 17:39:24 +0900

    test4

--- Chapter02/src/main/java/com/rubypaper/Chapter02Application.java
+++ Chapter02/src/main/java/com/rubypaper/Chapter02Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter02Application {
 
-	// git commit test3
+	// git commit test4
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter02Application.class, args);
 		System.out.println();
