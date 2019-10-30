e46b211a04cdf2491532517b5014f4bd5fa1a71a
ehddn0784 <ehddn0784@naver.com>
2019-10-30 13:48:34 +0900

    commit test_01

---
+++ Chapter03/src/main/java/com/example/demo/Chapter03Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter03Application.class, args);
+		
+		System.out.println("Chapter03");
+	}
+
+}
